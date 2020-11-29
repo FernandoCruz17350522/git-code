18fc310d050d4c688717532fea8145b6aac6ca87
Mateo <fernandocruzmateo502@gmail.com>
2020-11-29 13:40:29 -0600

    tercer video, hay que seguir en el 4000

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hola mundo!!!");
+const express = require('express');
+//servidor express
+const app = express();
+//rutas
+app.get('/', (req, res)=>{
+    console.log('señor, hemos recibido una petición en /');
+    res.json({
+        ok:true,
+        msg: "acaba de llegar!",
+    });
+});
+//configuracion
+app.listen(4000, () => {
+    console.log(`Backend corriendo en el puerto ${4000}`);
+})
