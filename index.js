a12e39d6c5badd6585e1f9c4916c212de3702f9a
Mateo <fernandocruzmateo502@gmail.com>
2020-11-29 16:56:56 -0600

    actualizacion del quito video, /routes/auth

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const express = require('express');
 //monitorear las variables de entorno(todas las variables)
 require('dotenv').config();
-
-//servidor express
 const app = express();
-
-
-
 //aceso al directorio publico
 app.use(express.static('public'));
-
-
-
 //rutas
-/*app.get('/', (req, res)=>{
-    res.json({
-        ok:true,
-        msg: "acaba de llegar!",
-    });
-});*/
+app.use('/api/auth', require('./routes/auth'));
 //configuracion
 app.listen(process.env.PORT, () => {
     console.log(`Backend corriendo en el puerto ${process.env.PORT}`);
